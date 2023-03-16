c4104a04ca856d81608daf9a39df3d016786c4d9
Andrei Pshenichnyi <andrey.pshenichniy.com>
2023-03-16 10:38:59 +0300

    Flake8 correctionsw

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.views.generic.base import TemplateView
 
 from .models import Group, Post, User
 from .forms import PostForm
